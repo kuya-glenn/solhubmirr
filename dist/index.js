b8e52aea4860d7c3f84a15127c714cf97b59f65e
Berna Oliva <berna@flint.com.ph>
2022-07-18 15:14:26 +0800

    initial commit

--- dist/index.js
+++ dist/index.js
@@ -5,10 +5,5 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
 const discord_1 = require("./modules/discord");
-if (fs_1.default.existsSync('./map.json')) {
-    discord_1.listen();
-}
-else {
-    discord_1.getChannels().then((channels) => discord_1.createServer(channels)).then(() => discord_1.listen());
-}
+
 //# sourceMappingURL=index.js.map
