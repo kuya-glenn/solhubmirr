629e6db0d7c45f39b34e4cd6b9d86f83db1a82df
kuya-glenn <66457439+kuya-glenn@users.noreply.github.com>
2022-07-18 15:44:19 +0800

    Add files via upload

--- dist/index.js
+++ dist/index.js
@@ -5,8 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
 const discord_1 = require("./modules/discord");
-
-discord_1.listen();
-
-
+if (fs_1.default.existsSync('./map.json')) {
+    discord_1.listen();
+}
+else {
+    discord_1.getChannels().then((channels) => discord_1.createServer(channels)).then(() => discord_1.listen());
+}
 //# sourceMappingURL=index.js.map
